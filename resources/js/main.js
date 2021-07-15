25250b919dcc10a3baa71023747028a825ed3bea
Jakub Kryska <kryska.jakub.optilean@outlook.com>
2021-07-16 01:46:48 +0200

    test

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,51 +1,36 @@
-
- (function() {
-            // https://dashboard.emailjs.com/admin/integration
-            emailjs.init('user_nTMwwmCm8qkC6drkM4iC7')
-        })();
+const serviceID = 'default_service';
+const templateID = 'template_brqgq58';	
+(function() {emailjs.init('user_nTMwwmCm8qkC6drkM4iC7')})();
 		
+	window.onload = function() {
+		const btn = document.getElementById('button');
+		document.getElementById('contact-form')
+		 .addEventListener('submit', function(event) {
+		   event.preventDefault();
+			const validationResult = this.checkValidity()
 
-        window.onload = function() {
-
+			if(validationResult === true) {
+				const oldButtonValue = btn.value
+				btn.value = "Wysyłanie informacji..."			
+				emailjs.sendForm(serviceID, templateID, this)
+				.then(() => {
+					const element = document.getElementById('formSent')
+					element.style.display = "block"
+					this.style.display = "none"
+				}, (err) => {
+					btn.value = oldButtonValue;
+					alert("Wystąpiły problemy w czasie wysyłania danych z formularza.")
+				});
+			 }
+	});            
+	Calendly.initBadgeWidget({ url: 'https://calendly.com/itlean', text: 'Umów się z nami na spotkanie', color: '#4d5055', textColor: '#ffffff', branding: false });					
+}	
 				
-			
-
-
-const btn = document.getElementById('button');
-
-document.getElementById('contact-form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
-
-   btn.value = 'Sending...';
-
-   const serviceID = 'default_service';
-   const templateID = 'template_brqgq58';
-
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
-    });
-});            
-        
-		
-		
-		
-		
-	
-				}
-				
-				
-				
-					 function myFunction() {
+function myFunction() {
     const x = document.getElementsByTagName("nav")[0];
     if (x.className != "responsive") {
       x.className = "responsive";
     } else {
       x.className = "";
     }
-  }
+}
